0d14437e91b74f9e63bb0448df5451c113fcc820
Martin Ptáček <marty.mptacek@gmail.com>
2020-08-20 11:38:07 +0200

    Websockets communication.

--- RemoteControl/src/main.cpp
+++ RemoteControl/src/main.cpp
@@ -5,23 +5,16 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <Webserver.h>
-#include <ArduinoWebsockets.h>
+#include <WebSocketsServer.h>   //https://github.com/Links2004/arduinoWebSockets
 #include <Wire.h>
 
 #include "StateVector.h"
 #include "TrackRay/TrackRay.h"
 #include "credentials.h"
 
-websockets::WebsocketsServer server;
+WebSocketsServer webSocket = WebSocketsServer(1337);
 WebServer webserver(80);
 WiFiManager wifiManager;
-
-void handleClients(void * param) {
-    for(;;) {
-        webserver.handleClient();
-        vTaskDelay(200);
-    }
-}
 
 StateVector stateVector;
 
@@ -30,17 +23,68 @@ uint32_t prevControlTime = 0;
 const uint16_t communicationTimeout = 1000;
 uint32_t prevCommunicationTime = 0;
 
-const uint16_t BLINK_PERIOD = 100;
+const uint16_t BLINK_PERIOD = 200;
 uint32_t prevBlinkTime = 0;
 
-//This function takes the parameters passed in the URL(the x and y coordinates of the joystick)
-void handleJSData() {
-    stateVector.joystickX = webserver.arg(0).toInt();
-    stateVector.joystickY = webserver.arg(1).toInt();
+// Callback: receiving any WebSocket message
+void onWebSocketEvent(uint8_t client_num, WStype_t type, uint8_t * payload, size_t length) {
 
-    //return an HTTP 200
-    webserver.send(200, "text/plain", "");
-    prevCommunicationTime = millis();
+    // Figure out the type of WebSocket event
+    switch(type) {
+
+        // Client has disconnected
+        case WStype_DISCONNECTED:
+            Serial.printf("[%u] Disconnected!\n", client_num);
+            break;
+
+        // New client has connected
+        case WStype_CONNECTED:
+        {
+            IPAddress ip = webSocket.remoteIP(client_num);
+            Serial.printf("[%u] Connection from ", client_num);
+            Serial.println(ip.toString());
+        }
+        break;
+
+        // Handle text messages from client
+        case WStype_TEXT:{
+            //printf("[%u] Received text: %s\n", client_num, payload);
+            const char delimiter[2] = ",";
+            char *token;
+            token = strtok((char *)payload, delimiter);
+            char controlMsg[] = "control";
+            if(strcmp(token, controlMsg) == 0) {
+                token = strtok(NULL, delimiter);
+                stateVector.joystickX = atoi(token);
+                token = strtok(NULL, delimiter);
+                stateVector.joystickY = atoi(token);
+                //printf("%d %d", stateVector.joystickX, stateVector.joystickY);
+                prevCommunicationTime = millis();
+            }
+        }
+
+
+
+            //webSocket.sendTXT(client_num, msg_buf);
+            break;
+
+        // For everything else: do nothing
+        /*case WStype_BIN:
+        case WStype_ERROR:
+        case WStype_FRAGMENT_TEXT_START:
+        case WStype_FRAGMENT_BIN_START:
+        case WStype_FRAGMENT:
+        case WStype_FRAGMENT_FIN:*/
+        default:
+            break;
+    }
+}
+
+void handleClients(void * param) {
+    for(;;) {
+        webserver.handleClient();
+        vTaskDelay(200);
+    }
 }
 
 void setup() {
@@ -68,10 +112,10 @@ void setup() {
     webserver.serveStatic("/", SPIFFS, "/webApp.html");
     webserver.serveStatic("/style.css", SPIFFS, "/style.css");
     
-    //call handleJSData function when this URL is accessed by the js in the html file
-    webserver.on("/jsData.html", handleJSData);
     webserver.begin();
-    server.listen(82);
+
+    webSocket.begin();
+    webSocket.onEvent(onWebSocketEvent);
 
     //xTaskCreate(handleClients, "handleClients", 1024 * 4 , (void*) 0, 1, NULL);
 }
@@ -80,8 +124,11 @@ void loop()
 {
     vTaskDelay(1);
     webserver.handleClient();
+
     if(millis() > prevControlTime + CONTROL_PERIOD) {
         prevControlTime = millis();
+        
+        webSocket.loop();
 
         if(millis() > prevCommunicationTime + communicationTimeout) {
             stateVector.joystickX = 0;
@@ -109,9 +156,6 @@ void loop()
             i = 0;
         trrSetLedDigital(i, 1);
         trrSetLedDigital(prevI, 0);
-        //trrSetFlashLightAnalog(i*4);
-        
-        //trrDisplayDigit(i);
         prevI = i;
     }
 }
